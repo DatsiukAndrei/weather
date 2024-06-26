83d94c2630fa59b08b0c7506e2396f1eb5826942
DatsiukAndrei <datsiuk07031991@gmail.com>
2024-06-26 21:21:59 +0200

    Fix build

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,19 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
-import { type PropsWithChildren } from "react";
-import "./tailwind.css";
+import { Outlet, Scripts } from "@remix-run/react";
 
-export function Layout({ children }: PropsWithChildren) {
+export function HydrateFallback() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="msapplication-TileColor" content="#da532c"/>
-        <meta name="theme-color" content="#ffffff"/>
-        <Meta />
-        <Links />
-        <title>Weather</title>
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
+    <>
+      <p>Loading...</p>
+      <Scripts />
+    </>
   );
 }
 
-
-
-function App() {
-  return <Outlet />;
+export default function Component() {
+  return (
+    <>
+      <Outlet />
+      <Scripts />
+    </>
+  );
 }
-
-export default App;
