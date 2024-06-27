58c1b0ba7bbab6fa548111dff22787dfbe4a8f25
DatsiukAndrei <datsiuk07031991@gmail.com>
2024-06-27 19:19:57 +0200

    Deployment

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export function Layout({ children }:PropsWithChildren) {
     <head>
       <meta charSet="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+      <link rel="shortcut icon" type="image/x-icon" href="/weather/favicon.ico"/>
       <Meta/>
       <Links/>
       <title>Weather</title>
